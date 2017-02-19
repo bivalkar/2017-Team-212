540eaf752d0d0a6ba4215c67f6bcbe9ef459eac9
alpita-masurkar <masurkar.alpita@gmail.com>
2017-02-19 15:06:19 -0800

    New print message on click

--- WarVeterans/app/src/main/java/sdplusplus/sdsu/edu/warveterans/MainActivity.java
+++ WarVeterans/app/src/main/java/sdplusplus/sdsu/edu/warveterans/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click_function(View view) {
-        System.out.println("Hello World");
+        System.out.println("New Branch");
     }
 }
