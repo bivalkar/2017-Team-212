26dfdd8f5c9b5fc1ce8622d973128b2203f6cd0c
kaivalya90 <deshpande.kaivalya90@gmail.com>
2017-02-19 17:19:55 -0800

    K's 1st

---
+++ app/src/main/java/sdplusplus/sdsu/edu/warveterans/MainActivity.java
@@ -0,0 +1,22 @@
+package sdplusplus.sdsu.edu.warveterans;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click_function(View view) {
+        System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.println("Nishant");
+
+        System.out.println("WOOWOWOW");
+    }
+}
